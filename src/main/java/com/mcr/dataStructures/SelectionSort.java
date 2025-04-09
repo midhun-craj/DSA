c248e92abdf8ea8b9ecd5c8083202d4d1a7fb673
midhun-craj <midhuncraj.m@gmail.com>
2025-04-09 18:01:03 +0530

    Learned the working of selection sort

---
+++ src/main/java/com/mcr/dataStructures/SelectionSort.java
@@ -0,0 +1,35 @@
+package com.mcr.dataStructures;
+
+public class SelectionSort {
+
+    /// Search through the array and keep track of the minimum value during each iteration.
+    /// And at the end of each iteration we swap the variables.
+    ///
+    /// Quadratic time O(n^2)
+    /// small datasets = okay
+    /// large datasets = bad
+
+    public static void selectionSort(int[] array) {
+
+        for(int i = 0; i < array.length - 1; i++) {
+            int min = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[min] > array[j]) {
+                    min = j;
+                }
+            }
+            int temp = array[i];
+            array[i] = array[min];
+            array[min] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int[] array = {8, 6, 1, 4, 2, 7, 9, 5, 3};
+
+        selectionSort(array);
+
+        for (int i : array) System.out.print(i + "\t");
+    }
+}
