48e2aa278b6af064ae85a50058e0ffa8cea085ca
midhun-craj <midhuncraj.m@gmail.com>
2025-04-14 22:08:44 +0530

    Learned about tree

---
+++ src/main/java/com/mcr/dataStructures/Tree.java
@@ -0,0 +1,15 @@
+package com.mcr.dataStructures;
+
+public class Tree {
+
+    /// A tree is a non-linear data structure where nodes are organized in a hierarchy.
+    /// ex: File explorer, Databases, DNS, HTML DOM
+    ///
+    /// A subtree is a smaller tree held within a larger tree.
+    /// Size of a tree is the no of nodes present in the tree.
+    ///
+    /// Depth of a node is the number of edges from the root node to that node.(root node has a depth of 0)
+    /// Height of a node is the number of edges on the longest path from that node down to a leaf node.(leaf node
+    /// has a height of 0)
+
+}
